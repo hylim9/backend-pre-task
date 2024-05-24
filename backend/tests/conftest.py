8099f62d9e3b139ae3d390ce4ef0e3d382f7705a
Chloe Lim <hylim9@gmail.com>
2024-05-24 15:12:08 +0900

    이미지변경

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -25,7 +25,7 @@ def create_contact(db):
             updated_by
         )
         VALUES (
-            'https://oopy.lazyrockets.com/api/rest/cdn/image/0196375a-2c5d-4ccd-afd8-d5034cc2cbe0.png',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnv42OT9xKWPPynLHlQPhYlIxWxxJNQBW7qbD-4AA5gQ&s',
             '테스트인물',
             'test@gmail.com',
             '010-1111-0000',
